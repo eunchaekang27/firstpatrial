7b2d2ceb8b26e41440ffd23fa6dfb2feb127a158
eunchaekang27 <145053331+eunchaekang27@users.noreply.github.com>
2023-10-17 12:17:29 -0600

    Create reto estudiantes.py

---
+++ reto estudiantes.py
@@ -0,0 +1,13 @@
+print("Hi, I'm a program that evaluates students")
+av_grade = int(input("Input your average grade from 0-100"))
+c_participation = input("Is your classroom participation good? (Answer with yes or no)")
+p_score = int(input("Insert your project score from 0-100"))
+
+if av_grade >= 75 and c_participation == "yes":
+  print("You are in a good academic standing.")
+
+if p_score > 90:
+  print("You received a distinction.")
+
+if av_grade < 60 or c_participation == "no":
+  print("You need to improve your performance")
